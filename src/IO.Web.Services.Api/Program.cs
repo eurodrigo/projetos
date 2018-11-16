09332f3ecf4a1857a5ce6b2d6fc4a4815287a98f
atvmsoft <atvmsoft@gmail.com>
2018-11-16 17:24:29 -0200

    Remove Comment

--- src/IO.Web.Services.Api/Program.cs
+++ src/IO.Web.Services.Api/Program.cs
@@ -10,26 +10,11 @@ namespace IO.Web.Services.Api
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
-            //var host = new WebHostBuilder()
-            //    .UseKestrel()
-            //    .UseContentRoot(Directory.GetCurrentDirectory())
-            //    .UseIISIntegration()
-            //    .UseStartup<Startup>()
-            //    .UseApplicationInsights()
-            //    .Build();
-
-            //host.Run();
-
-            //CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //        .UseStartup<Startup>();
     }
 }
