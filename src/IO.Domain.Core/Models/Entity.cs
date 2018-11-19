8d65a1903331c807162c13f9f8d02054c253ef00
eurodrigo <eu.rodrigo@gmail.com>
2018-11-19 11:50:29 -0200

    #

--- src/IO.Domain.Core/Models/Entity.cs
+++ src/IO.Domain.Core/Models/Entity.cs
@@ -21,6 +21,7 @@ namespace IO.Domain.Core.Models
             var compareTo = obj as Entity<T>;
 
             if (ReferenceEquals(this, compareTo)) return true;
+
             if (ReferenceEquals(null, compareTo)) return false;
 
             return Id.Equals(compareTo.Id);
@@ -29,6 +30,7 @@ namespace IO.Domain.Core.Models
         public static bool operator ==(Entity<T> a, Entity<T> b)
         {
             if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+
             if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
 
             return a.Equals(b);
