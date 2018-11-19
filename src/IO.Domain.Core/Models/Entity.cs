17234c7c8d40f2ea6fb379a60b976cb31883f43a
eurodrigo <eu.rodrigo@gmail.com>
2018-11-19 11:45:42 -0200

    #

--- src/IO.Domain.Core/Models/Entity.cs
+++ src/IO.Domain.Core/Models/Entity.cs
@@ -19,7 +19,9 @@ namespace IO.Domain.Core.Models
         public override bool Equals(object obj)
         {
             var compareTo = obj as Entity<T>;
+
             if (ReferenceEquals(this, compareTo)) return true;
+
             if (ReferenceEquals(null, compareTo)) return false;
 
             return Id.Equals(compareTo.Id);
@@ -28,6 +30,7 @@ namespace IO.Domain.Core.Models
         public static bool operator ==(Entity<T> a, Entity<T> b)
         {
             if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+
             if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
 
             return a.Equals(b);
